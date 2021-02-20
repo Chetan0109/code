67af3938b9a7e92d7b7e9a24509a27ce732bbdbc
Chetan0109 <79197628+Chetan0109@users.noreply.github.com>
2021-02-20 14:47:18 +0530

    Add files via upload

---
+++ StringCal.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class StringCal{
+	public static void main(String args[]){
+		ReturnZeroIfNull();
+	}
+	
+	public static int add(String str){
+		return 0;
+	}
+	
+	public static void ReturnZeroIfNull(){
+		int sum = 0;
+		int val;
+		val = StringCal.add("");
+		if(sum == val){
+			System.out.println("OK");
+		}
+		else{
+			System.out.println("NOT OK");
+		}
+	}
+}
