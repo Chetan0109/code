073763fdf08da5c150398b1e24bfb4ca314ad63b
Chetan0109 <79197628+Chetan0109@users.noreply.github.com>
2021-02-20 14:57:13 +0530

    Update StringCal.java

--- StringCal.java
+++ StringCal.java
@@ -3,9 +3,11 @@ import java.io.*;
 public class StringCal{
 	public static void main(String args[]){
 		ReturnZeroIfNull();
+		ReturnNumbersIfNotNull();
 	}
 	
 	public static int add(String str){
+		
 		return 0;
 	}
 	
@@ -13,6 +15,17 @@ public class StringCal{
 		int sum = 0;
 		int val;
 		val = StringCal.add("");
+		compare(sum,val);
+		
+	}
+	
+	public static void ReturnNumbersIfNotNull(){
+		int sum = 12;
+		int val = StringCal.add("12");
+		compare(sum,val);
+	}
+	
+	public static void compare(int sum, int val){
 		if(sum == val){
 			System.out.println("OK");
 		}
@@ -20,4 +33,5 @@ public class StringCal{
 			System.out.println("NOT OK");
 		}
 	}
+		
 }
