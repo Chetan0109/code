91cb06a26b3b2e2859c88013f5589e25d983d620
Chetan0109 <motwanichetan9@gmail.com>
2021-04-02 12:07:31 +0100

    4th - spliited columns in database

--- data_load.py
+++ data_load.py
@@ -7,9 +7,13 @@ def hello_world():
 	return 'hello world'
 	
 def load_excel():
-	df = pd.read_excel('customer.xls',header=0)
-	print(df)
-	index = df.index
+	df = pd.read_excel('customer.xls',header=0, separator = '|')
+	
+	df2 = pd.DataFrame(df['|H|Customer_Name|Customer_Id|Open_Date|Last_Consulted_Date|Vaccination_Id|Dr_Name|State|Country|DOB|Is_Active'].str.split('|').tolist())
+	df2.columns = ['chetan','H', 'Customer_Name', 'Customer_Id','Open_Date','Last_Consulted_Date','Vaccination_Id','Dr_Name','State','Country','DOB','Is_Active']
+	df2 = df2.drop(['chetan'], axis = 1)
+	print(df2)
+	index = df2.index
 	number_of_rows = len(index)
 	return number_of_rows
 
