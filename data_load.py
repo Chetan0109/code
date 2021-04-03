4ecdbfb6857af4386eb7d0ea9cd61ac35344d102
Chetan0109 <motwanichetan9@gmail.com>
2021-04-03 19:39:34 +0100

    6th - final code

--- data_load.py
+++ data_load.py
@@ -1,10 +1,8 @@
 import unittest
 import pandas as pd
 import teradata
-#from mycode import *
 
-
-
+host,username,password = '#######','######', '#######'
 
 def hello_world():
 	return 'hello world'
@@ -15,7 +13,8 @@ def load_excel():
 	df2 = pd.DataFrame(df['|H|Customer_Name|Customer_Id|Open_Date|Last_Consulted_Date|Vaccination_Id|Dr_Name|State|Country|DOB|Is_Active'].str.split('|').tolist())
 	df2.columns = ['chetan','H', 'Customer_Name', 'ID','Open_Date','Last_Consulted_Date','Vaccination_Id','Dr_Name','State','Country','DOB','Is_Active']
 	df2 = df2.drop(['chetan','H'], axis = 1)
-	#print(df2)
+	
+	#Formatting date columns
 	df2['Open_Date'] = df2['Open_Date'].str[0:4] + "/" + df2['Open_Date'].str[4:6] + "/" + df2['Open_Date'].str[6:8]
 	df2['Last_Consulted_Date'] = df2['Last_Consulted_Date'].str[0:4] + "/" + df2['Last_Consulted_Date'].str[4:6] + "/" + df2['Last_Consulted_Date'].str[6:8]
 	df2['DOB'] = df2['DOB'].str[0:2] + "/" + df2['DOB'].str[2:4] + "/" + df2['DOB'].str[4:8]
@@ -28,7 +27,6 @@ def load_excel():
 def insertIntoTable(df2):
 	print('entered here')
 	udaExec = teradata.UdaExec (appName="test", version="1.0", logConsole=False)
-	host,username,password = '#######','######', '#######'
 	with udaExec.connect(method="odbc",system=host, username=username,password=password, driver="Teradata") as connect:
 		print('entered')
 		df3=pd.read_sql("select count(*) from dev_wrk.customers_tmp",connect)
@@ -44,9 +42,54 @@ def insertIntoTable(df2):
 			return 'pass'
 		except Exception as e:
 			print(e)
-			#pass
-		#finally:
-			#return number_of_rows
+
+			
+def Load_Final_Data():
+	print("Hi")
+	udaExec = teradata.UdaExec (appName="test", version="1.0", logConsole=False)
+	with udaExec.connect(method="odbc",system=host, username=username,password=password, driver="Teradata") as connect:
+		df = pd.read_sql("select distinct country from dev_wrk.customers_tmp",connect)
+		df.columns = ['country']
+
+		for i in range(len(df)):
+			COUNTRY_CODE = str(df["country"].loc[i])
+
+			cursor = connect.cursor()
+			select_query = "select distinct country from dev_wrk.countries where country_code = '" + COUNTRY_CODE + "'" 
+
+			cursor.execute(select_query)
+			# get all records
+			records = cursor.fetchall()
+			for row in records:
+				str1 = row[0]
+				#Check Main table presence and fetch the tablename
+				table = checkTablePresence(str1)
+				#inserting the data into final tables based on country
+				insert_query = "INSERT INTO DEV_WRK." + table + "SEL * FROM DEV_WRK.CUSTOMERS_TMP WHERE COUNTRY = '"+ COUNTRY_CODE + "'"
+				cursor.execute(insert_query)
+			#print("Total number of rows in table: ", cursor.rowcount)
+	return "pass"
+	
+def checkTablePresence(str1):
+	table_name = 'TABLE_' + str1
+	udaExec = teradata.UdaExec (appName="test", version="1.0", logConsole=False)
+	with udaExec.connect(method="odbc",system=host, username=username,password=password, driver="Teradata") as connect:
+		cursor = connect.cursor()
+		sel_query = "select count(*) from dbc.tables where tablename = '" + table_name + "' and databasename = 'DEV_WRK'"
+		create_query = "CREATE SET TABLE DEV_WRK." + table_name +",FALLBACK ,NO BEFORE JOURNAL,NO AFTER "+"JOURNAL,CHECKSUM = DEFAULT,DEFAULT MERGEBLOCKRATIO(CUSTOMER_NAME VARCHAR(255) CHARACTER SET LATIN NOT"+" CASESPECIFIC NOT NULL,CUSTOMER_ID VARCHAR(18) CHARACTER SET LATIN NOT CASESPECIFIC NOT "+"NULL,CUSTOMER_OPEN_DATE DATE FORMAT 'YYYY/MM/DD' NOT NULL,LAST_CONSULTED_DATE DATE FORMAT "+"'YYYY/MM/DD',VACCINATION_TYPE CHAR(5) CHARACTER SET LATIN NOT CASESPECIFIC,Doctor_Consulted CHAR(255)"+" CHARACTER SET LATIN NOT CASESPECIFIC,State CHAR(5) CHARACTER SET LATIN NOT CASESPECIFIC,Country"+" CHAR(5) CHARACTER SET LATIN NOT CASESPECIFIC,Post_Code INTEGER,DATE_OF_BIRTH DATE FORMAT 'MM/DD/YYYY',"+"ACTIVE_CUSTOMER CHAR(1) CHARACTER SET LATIN NOT CASESPECIFIC)PRIMARY INDEX ( CUSTOMER_ID,CUSTOMER_NAME );"
+
+		cursor.execute(sel_query)
+		# get all records
+		records = cursor.fetchall()
+		for row in records:
+			
+			if(row[0] == 0):
+				print("table not present")
+				#creating the required table
+				cursor.execute(create_query)
+	return table_name
+				
+	
 
 
 class MyFirstTests(unittest.TestCase):
@@ -57,8 +100,8 @@ class MyFirstTests(unittest.TestCase):
 	def test_data(self):
 		self.assertEqual(load_excel(),5)
 		
-	#def test_table(self):
-		#self.assertEqual(insertIntoTable(),'pass')
+	def test_table(self):
+		self.assertEqual(Load_Final_Data(),'pass')
 
 
 		
